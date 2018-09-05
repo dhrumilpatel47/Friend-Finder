18b8f8f82983b2c0862c5d03adc35d7829de8aac
dhrumilpatel47 <dhrumilpatel47@gmail.com>
2018-09-04 21:08:57 -0400

    homework

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,23 +7,26 @@ module.exports = function (app) {
     });
 
     app.post('/api/friends', function (req, res) {
-        var newFriend = req.body;
-        var newScores = newFriend.scores;
-        var minTotalDifference = 1000;
-        var result = "";
-
+        var input = req.body;
+        var responses = input.scores;
+        var yourFriend = "";
+        var yourFriendImage = "",
+        var differance = 1000;
+        
+        
         for (var i = 0; i < friends.length; i++) {
             var totalDifference = 0;
-            for (var j = 0; j < newScores.length; j++) {
-                totalDifference += Math.abs(parseInt(newScores[j]) - parseInt(friends[i].scores[j]));
+            for (var j = 0; j < responses.length; j++) {
+                totalDifference += Math.abs(parseInt(responses[j]) - parseInt(friends[i].scores[j]));
             }
-            if (totalDifference < minTotalDifference) {
-                minTotalDifference = totalDifference;
-                result = friends[i].name;
+            if (totalDifference < differance) {
+                differance = totalDifference;
+                yourFriend = friends[i].name;
+                yourFriendImage= friends[i].name;
             }
         }
         
-        friends.push(newFriend);
-        res.json(result);
+        friends.push(input);
+        res.json(yourFriend);
     })
 }
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 app.use(express.static('app/public'));
 
